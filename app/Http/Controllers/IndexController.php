c6269e6787bc49802ba1d4616ca7e95b02154452
Irlando José Bezerra <irlandoflyboyifceuece@gmail.com>
2026-01-20 17:42:21 -0300

    Corrige IndexController e carrega config_site

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Controllers\DB;
+use Illuminate\Support\Facades\DB; ✅ CERTO
 /**
  * Index Page Controller
  * @category  Controller
